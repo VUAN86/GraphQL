87dada0496f411759dd4b891a2d0999e92e75f1f
Вуколов Андрей Владимирович <avukolov@mpl.lc>
2020-12-23 12:17:14 +0300

    new repo

---
+++ graphql-api/src/main/java/ru/vuan/graphql/api/GraphQLProvider.java
@@ -0,0 +1,47 @@
+package ru.vuan.graphql.api;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
+
+@Component
+public class GraphQLProvider {
+
+    @Autowired
+    GraphQLDataFetchers graphQLDataFetchers;
+
+    @Bean
+    GraphQLSchema schema() throws IOException{
+        var url = Resources.getResource("schema.graphqls");
+        var sdl = Resources.toString(url, Charsets.UTF_8);
+        return buildSchema(sdl);
+    }
+
+
+    private GraphQLSchema buildSchema(String sdl) {
+        var typeRegistry = new SchemaParser().parse(sdl);
+        var runtimeWiring = buildWiring();
+        var schemaGenerator = new SchemaGenerator();
+        return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+    }
+
+    private RuntimeWiring buildWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                .type(newTypeWiring("Query")
+                        .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
+                .type(newTypeWiring("Book")
+                        .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher())
+                        .dataFetcher("pageCount", graphQLDataFetchers.getPageCountDataFetcher()))
+                .build();
+    }
+}
